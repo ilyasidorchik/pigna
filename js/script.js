18863b28e295a40b4d464eba907f75a0061e3860
Ilya Sidorchik <ilyasidorchik@MacBook-Pro-Ilya.local>
2019-02-24 17:35:18 +0300

    Better input year: reduced and only for numbers

--- js/script.js
+++ js/script.js
@@ -31,6 +31,26 @@ function start() {
 
     let publishingYearInput = document.querySelector('.form__element__input-publishing-year');
 
+
+    // Поле с годом издания только для цифр
+    publishingYearInput.onkeypress = function(e) {
+        e = e || event;
+
+        if (e.ctrlKey || e.altKey || e.metaKey) return;
+
+        var chr = getChar(e);
+
+        // с null надо осторожно в неравенствах,
+        // т.к. например null >= '0' => true
+        // на всякий случай лучше вынести проверку chr == null отдельно
+        if (chr == null) return;
+
+        if (chr < '0' || chr > '9') {
+            return false;
+        }
+    }
+
+
     authorInput.addEventListener('keyup',()=>{
         // При заполнении полей появляется обложка
         // Если поля снова очищаются, обложка пропадает
@@ -308,4 +328,18 @@ function addBook() {
     };
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.send(params);
+}
+
+function getChar(event) {
+    if (event.which == null) {
+        if (event.keyCode < 32) return null;
+        return String.fromCharCode(event.keyCode) // IE
+    }
+
+    if (event.which != 0 && event.charCode != 0) {
+        if (event.which < 32) return null;
+        return String.fromCharCode(event.which) // остальные
+    }
+
+    return null; // специальная клавиша
 }
