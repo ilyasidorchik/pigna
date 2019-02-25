0a88204a423616c0c63de0b041270f41862bd7ab
Ilya Sidorchik <ilyasidorchik@MacBook-Pro-Ilya.local>
2019-02-25 12:38:29 +0300

    Replacement of old month book by new

--- php/addBook.php
+++ php/addBook.php
@@ -15,4 +15,9 @@
     $link = mysqli_connect($ini[database][host], $ini[database][user], $ini[database][password], $ini[database][name]) or die('Ошибка');
     mysqli_set_charset($link, 'utf8');
 
+    // При добавлении новой книги месяца, старая книга месяца теряет свой статус
+    if ($monthBook == 1)
+        mysqli_query($link, "UPDATE catalogue SET monthBook = 0 WHERE monthBook = 1");
+
+    // Добавление новой книги в базу данных
     mysqli_query($link, "INSERT INTO catalogue (id, title, author, publishing, price, monthBook, description) VALUES (NULL, '$title', '$author', '$publishing', '$price', '$monthBook', '$description')");
